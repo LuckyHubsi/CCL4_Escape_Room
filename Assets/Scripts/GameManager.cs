aad8dae820ff3fc09e0b3bf82e7d54d032fab926
LuckyHubsi <cc221040@fhstp.ac.at>
2024-06-11 11:49:58 +0200

    Json integration for randomising the torch color implemented

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,53 +4,17 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance { get; private set; }
-    private Dictionary<string, Vector3> _originalPositions = new Dictionary<string, Vector3>();
+    public static GameManager instance;
 
-    private void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
-        // Singleton pattern implementation
-        if (Instance == null)
+        if (instance != null)
         {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
+            return;
         }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
 
-    // Add a method to store original positions
-    public void StoreOriginalPosition(string objectId, Vector3 position)
-    {
-        if (!_originalPositions.ContainsKey(objectId))
-        {
-            _originalPositions.Add(objectId, position);
-        }
-        else
-        {
-            _originalPositions[objectId] = position;
-        }
-    }
-
-    // Add a method to reset object position
-    public void ResetObjectPosition(GameObject obj)
-    {
-        string objectId = obj.name; // You can use a unique identifier for the object
-        if (_originalPositions.ContainsKey(objectId))
-        {
-            obj.transform.position = _originalPositions[objectId];
-        }
-        else
-        {
-            Debug.LogError("Original position not found for object: " + objectId);
-        }
-    }
-
-    // Add a method to load positions from JSON
-    public void LoadPositionsFromJSON(string json)
-    {
-        // Implement your JSON parsing logic here to load positions
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
     }
 }
