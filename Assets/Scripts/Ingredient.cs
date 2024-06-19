22a4a38fd16cb878210af3d51743cfbbda564f66
LuckyHubsi <cc221040@fhstp.ac.at>
2024-06-19 21:53:25 +0200

    Added ingredients and edited prefab

--- Assets/Scripts/Ingredient.cs
+++ Assets/Scripts/Ingredient.cs
@@ -8,6 +8,37 @@ public class Ingredient : Interactable
     [SerializeField]
     public IngredientState ingredientState = IngredientState.Empty;
 
+    [SerializeField]
+    private Material matLid_Bat;
+    [SerializeField]
+    private Material matLid_Eye;
+    [SerializeField]
+    private Material matLid_Tongue;
+    [SerializeField]
+    private Material matLid_ToothWrong1;
+    [SerializeField]
+    private Material matLid_ToothWrong2;
+    [SerializeField]
+    private Material matLid_ToothRight;
+
+    [SerializeField]
+    private Material mat_Bat;
+    [SerializeField]
+    private Material mat_Eye;
+    [SerializeField]
+    private Material mat_Tongue;
+    [SerializeField]
+    private Material mat_Tooth;
+
+    [SerializeField]
+    private Mesh bat;
+    [SerializeField]
+    private Mesh eye;
+    [SerializeField]
+    private Mesh tongue;
+    [SerializeField]
+    private Mesh tooth;
+
     private PlayerInteraction _playerInteraction;
 
     private void Start()
@@ -27,5 +58,29 @@ public class Ingredient : Interactable
     public void SetIngredientState(IngredientState newState)
     {
         ingredientState = newState;
+    }
+
+    private void UpdateIngredientAppearance()
+    {
+        switch (ingredientState)
+        {
+            case IngredientState.Empty:
+                return;
+            case IngredientState.BatWing:
+                this.gameObject.GetComponent<Renderer>().material = matRed;
+                break;
+            case IngredientState.Eye:
+                this.gameObject.GetComponent<Renderer>().material = matBlue;
+                break;
+            case IngredientState.FroshTongue:
+                this.gameObject.GetComponent<Renderer>().material = matYellow;
+                break;
+            case IngredientState.Tooth_Wrong:
+                this.gameObject.GetComponent<Renderer>().material = matPurple;
+                break;
+            case IngredientState.Tooth_Correct:
+                this.gameObject.GetComponent<Renderer>().material = matGreen;
+                break;
+        }
     }
 }
