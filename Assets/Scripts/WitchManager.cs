079bb87d4726954af084e71e12575da1331ffc1f
cc221027 <cc221027@fhstp.ac.at>
2024-06-20 14:27:24 +0200

    Fixed barrier and changed color and also added barrier variant

--- Assets/Scripts/WitchManager.cs
+++ Assets/Scripts/WitchManager.cs
@@ -27,7 +27,7 @@ public class WitchManager : MonoBehaviour
     {
         if (playerInBox)
         {
-           if(witch.currentWitchState == 4)
+           if(witch.currentWitchState == 4 && ProgressionManager.instance.puzzleOneSolved)
             {
                 witch.isWatching = true; // Continue watching
                // Debug.Log("Witch is watching");
@@ -50,13 +50,11 @@ public class WitchManager : MonoBehaviour
     public void PlayerEnteredBox()
     {
         playerInBox = true; // Player entered the detection box
-        Debug.Log("I CAN SEE YOU");
     }
     public void PlayerExitedBox()
     {
         playerInBox = false; // Player left the detection box
         StartCoroutine(StopWatchingAfterDelay(3f)); // Ensure witch stops watching after 3 seconds
-        Debug.Log("I CANNOT SEE YOU");
     }
 
 
